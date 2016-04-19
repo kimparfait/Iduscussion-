b2f0c0fd866971a6053bf3f461964e4bb8208225
kim <kimtalya7@gmail.com>
2016-04-20 01:16:13 +0300

    add name to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419165222) do
+ActiveRecord::Schema.define(version: 20160419220804) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "message"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160419165222) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
